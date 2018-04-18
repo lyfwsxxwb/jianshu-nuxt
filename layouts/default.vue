46272ca9ee608b952159e58d3b4a7edc11d9a30a
xwb <1356193003@qq.com>
2018-04-18 14:50:36 +0800

    创建了index.vue页面

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,16 +5,16 @@
 </template>
 
 <script>
-  import Vue from 'vue'
-  import BootstrapVue from 'bootstrap-vue'
-  Vue.use(BootstrapVue)
-  //首先引入bootstrap.css文件，加载bootstrap样式
-  import 'bootstrap/dist/css/bootstrap.css'
-  //引入bootstrap-vue.css文件
-  import 'bootstrap-vue/dist/css/bootstrap-vue.css'
-  export default {
+    import Vue from 'vue'
+    import BoostrapVue from 'bootstrap-vue'
+    Vue.use(BoostrapVue)
+    //首先引入bootstrap.css文件，加载bootstrap样式
+    import 'bootstrap/dist/css/bootstrap.css'
+    //引入bootstrap-vue.css文件
+    import 'bootstrap-vue/dist/bootstrap-vue.css'
+    export default {
 
-}
+    }
 </script>
 
 
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>hello world</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                name:'首页'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
