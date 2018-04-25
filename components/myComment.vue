c51fa55ac516627d8db6b04c2d489c246582d9b4
xwb <1356193003@qq.com>
2018-04-25 15:12:59 +0800

    更多和分享样式完成

---
+++ components/myComment.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "myComment",
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/p/_id.vue
@@ -0,0 +1,128 @@
+<template>
+    <div>
+        <my-header></my-header>
+        <div class="note">
+            <div class="post">
+
+                <div class="article">
+                    <h1 class="title">芳华--小萍，愿你被世界温柔以待</h1>
+                    <div class="author">
+                        <div class="avatar">
+                            <img src="~assets/img/default-avatar.jpg">
+                        </div>
+                        <div class="info">
+                            <nuxt-link class="name" to="name">
+                                林十一姐
+                            </nuxt-link>
+                            <a href="#" class="btn btn-follow">
+                                <i class="fa fa-plus"></i>
+                                <span>关注</span>
+                            </a>
+                            <div class="meta">
+                            <span class="publish-time">
+                                2018.03.15 09:33
+                            </span>
+                                <span class="wordage">
+                                字数 1287
+                            </span>
+                                <span class="view-count">
+                                阅读 18806
+                            </span>
+                                <span class="comment-count">
+                                 评论 253
+                            </span>
+                                <span class="like-count">
+                                 喜欢 401
+                            </span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--文章正文-->
+                    <div class="show-content">
+                        <p>我已经很久不去电影院，但是《芳华》预告出来后，我一直很期待。虽然冯导上部《我不是潘金莲》让人略感失望，但他仍是我最喜爱的导演之一。</p><p>因为没有合适的同伴，我是一个人去看的。看的时候几次要落泪。</p><p>我不喜欢写影评。在我内心里无论怎么欢欣愉悦愤慨恼怒，写评论时总会习惯性克制的波澜不惊，可能感情在观看时已经宣泄完，任我手写我心，却再也写不出当时的心情。<br></p><p>导演为了有矛盾冲突，设计出大家排挤小萍的原因，因为她土，没背景。这种孤立在生活中有很多，但电影中最残酷的就在于她被下放了，下放到了特殊时期的战争中。当其他人都享受着青春，放声歌唱的时候，她和刘峰面对的是血肉横飞的战场。他们到底做错了什么？没有。为什么要比别人过的更辛苦？因为善良？是的，命运从不会因为你善良而眷顾。</p><p>看影评，每个人都有自己的解读。你带着什么心，就会看到什么。有人在电影里看到了阶层，有人看到了青春，我看到了人性。我爸是那个年代的炮兵连长，差一点就上了战场。他看这个电影，感触更多的是战争的残酷和对牺牲战友的怀念。这是我第一次看到关于这场战争这么具体的描绘，炮火连天，尸横遍野，残肢断臂。隔着屏幕，似乎都闻到了浓浓的血腥味。<br></p><p>是的，我喜欢小萍，同情刘峰，因为他们是这场青春的牺牲者。当芳华已逝，留给他们的只有残缺的身体和破碎的心。他们怀着一颗善良单纯的心，却始终不被善待。所以，我同情所有努力生活却遭遇不公的人，被周围人排挤欺负却不会辩解的人。我在他们身上看到了年少时的自己。<br></p><p>上五年级时，我从乡下转到了城里独自求学。到了陌生的环境，我一边努力学习，一边努力融入班级，给同学讲题，帮同学买东西。可是不管我怎么向她们示好，讨好她们，还是被她们取笑，每天被同桌挤得只能趴课桌边写字。女生都孤立我，没人和我玩。只有偶尔她们需要帮忙时才叫我:一一，过来。<br></p><p>这些情况老师都看到了，可是他们都像影片中的团长，选择性忽视。直到有一天我忍无可忍，和一个女生打架。她比我高半头，那天又无缘无故把我的书包和文具扔在了地上，然后挑衅地看着我，没有人帮我。我忽然怒了，抓起一盒粉笔朝她砸去。最后我俩扭成一团，滚在地上，被老师叫到了办公室。老师虽然批评了我俩，但是明显是冲着我，让我先道歉。那时年幼的我，沉默倔强，头发被扯，一身的抓痕，任老师怎么斥责，强忍着泪水，始终不发一言。</p><p>但是从那时起，班里人再没人欺负我，从那时起，我就知道，你可以善良，但不要软弱，要学会自己保护自己。</p><p>所以，林丁丁为了保护自己，诬陷了刘峰；萧穗子为了保护自己，虽然同情小萍，但在所有人诬陷小萍时，并没有挺身而出，她始终选择和郝淑雯们站在一起。看到一篇文章中写到:“霸凌者永远意识不到自己的恶意。”意识不到吗？在影片中推动他们命运的政委和团长没有意识到吗？不，团长看到了视而不见，政委更是利用完小萍演出后，以冠冕堂皇的理由一手把她送到了前线。无论刘峰和小萍怎么努力，他们最终还是被这个集体无情的抛弃。</p><p>最后直到在战场上，刘峰与小萍才得到了信任与尊重。</p><p>我不想责怪她们，每个人都有自己的困境，每个人都有权利做自己的选择。我只想说:你可以不善良，但不要欺负善良。</p><p>而你们，欠他们一个“芳华”。</p>
+                    </div>
+
+                </div>
+                <!--底部文章分类信息和版权-->
+                <div class="show-foot">
+                    <nuxt-link to="/note/123" class="notebook">
+                        <i class="fa fa-book"></i>日记本
+                    </nuxt-link>
+                    <div class="copyright">
+                        © 著作权归作者所有
+                    </div>
+                </div>
+                <!--文章底部的作者信息-->
+                <div class="follow-detail">
+                    <div class="info">
+                        <div class="avatar">
+                            <img src="~assets/img/default-avatar.jpg">
+                        </div>
+                        <a href="#" class="btn-follow">
+                            <i class="fa fa-plus"></i>
+                            <span>关注</span>
+                        </a>
+                        <nuxt-link class="title" to="/u/123">
+                            雪笨笨
+                        </nuxt-link>
+                        <i class="fa fa-venus"></i>
+                        <p>写了 1199 字，被 4 人关注，获得了 611 个喜欢</p>
+                    </div>
+                </div>
+                <!--文章底部的喜欢和分享-->
+                <div class="meta-bottom">
+                    <div class="like">
+                        <div class="like-btn">
+                            <a href="#" class="">喜欢</a>
+                        </div>
+                        <div class="like-num">
+                            <a href="#">50</a>
+                        </div>
+                    </div>
+                    <div class="share">
+                        <a href="#" class="share-btn">
+                            <i class="fa fa-weixin weixin"></i>
+                        </a>
+                        <a href="#" class="share-btn">
+                            <i class="fa fa-weibo weibo"></i>
+                        </a>
+                        <a href="#" class="share-btn">
+                            <i class="fa fa-qq qq"></i>
+                        </a>
+                        <a href="#" class="more-share share-btn">
+                            更多分享
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!--留言分享-->
+            <my-comment></my-comment>
+
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    import myComment from '~/components/myComment'
+    import myHeader from '~/components/myHeader'
+    export default {
+        head:{
+            title:"简书文章详情页面-创作你的创作",
+            meta:[
+                {charset:'utf-8'},
+                {name:'keywords',content:'文章详情页面'},
+                {name:'description',content:'文章页面'},
+            ]
+        },
+        data(){
+            return{
+
+            }
+        },
+        components:{
+            myHeader,
+            myComment
+        }
+    }
+</script>
